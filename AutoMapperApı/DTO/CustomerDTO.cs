220d62003dc006b8e478034af3de310cabe2d76e
gabrieljulien12 <153957457+gabrieljulien12@users.noreply.github.com>
2024-05-03 17:44:34 +0300

    Update CustomerDTO.cs

--- AutoMapperApı/DTO/CustomerDTO.cs
+++ AutoMapperApı/DTO/CustomerDTO.cs
@@ -3,8 +3,7 @@
     public class CustomerDTO
     {
         public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string FullName { get; set; }
         public int Age { get; set; }
         public string EmailAdres { get; set; }
     }
